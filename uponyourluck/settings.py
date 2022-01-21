e1f85b2046fc2b5d8097feae7c0e354c73adbaf6
nebula1989 <benwalterscoding@gmail.com>
2022-01-21 14:45:54 -0800

    created signup modelform

--- uponyourluck/settings.py
+++ uponyourluck/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap5',
     'welcome',
-    'registration',
+    'registration.apps.RegistrationConfig',
 ]
 
 MIDDLEWARE = [
--- uponyourluck/urls.py
+++ uponyourluck/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('welcome.urls')),
     path('registration/', include('registration.urls')),
