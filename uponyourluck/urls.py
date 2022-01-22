bc09d5cbba012765b96ce6a8f57d22aa81f31425
nebula1989 <benwalterscoding@gmail.com>
2022-01-22 11:35:24 -0800

    registration using built in django tools

--- uponyourluck/urls.py
+++ uponyourluck/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from uponyourluck import views
 
 urlpatterns = [
     path('', include('welcome.urls')),
+    path("register", views.register_request, name="register"),
     path('admin/', admin.site.urls),
 ]
