4c1567bce0105fe4343a8a408656436010015259
nebula1989 <benwalterscoding@gmail.com>
2022-02-06 18:49:46 -0800

    created QR code sticker index page

--- stickers/views.py
+++ stickers/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def sticker_index(request):
-    return render(request, 'sticker_index.html')
+    args1 = {
+        'current_user': request.user,
+    }
+    return render(request, 'sticker_index.html', args1)
