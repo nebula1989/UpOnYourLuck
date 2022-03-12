8642b0e1ece353ab7edb261e4655c3c38e8bc5b5
nebula1989 <benwalterscoding@gmail.com>
2022-03-12 07:07:50 -0800

    adjusted size of qr print, minor formatting

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ import os
 from textwrap import fill
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ChangePassword, LoginForm, NewUserForm, UpdateProfileForm, UpdateUserForm #UpdateUserForm
+from .forms import ChangePassword, LoginForm, NewUserForm, UpdateProfileForm, UpdateUserForm  # UpdateUserForm
 from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
@@ -28,6 +28,7 @@ def dashboard(request):
 
     return render(request, 'dashboard.html', context)
 
+
 @login_required()
 def view_followers(request):
     followers_list = FollowersCount.objects.filter(follower=request.user.username)
@@ -43,6 +44,7 @@ def view_followers(request):
         'title': "Your followers"
     }
     return render(request, 'welcome/show_all_users.html', context)
+
 
 @login_required()
 def view_following(request):
@@ -61,6 +63,7 @@ def view_following(request):
     }
     return render(request, 'welcome/show_all_users.html', context)
 
+
 # for visitors going to a user's profile page
 def visitor_to_profile(request, username=None):
     if username:
@@ -76,14 +79,14 @@ def visitor_to_profile(request, username=None):
         print(profile.qr_scan_count)
         profile.save()
 
-    #Get number of followers and following
+    # Get number of followers and following
     current_user = username_obj.username
     logged_in_user = request.user.username
     user_followers = len(FollowersCount.objects.filter(follower=current_user))
     user_following = len(FollowersCount.objects.filter(following=current_user))
     follower_list = FollowersCount.objects.filter(following=current_user)
 
-    #Loop through follower_list to check if requester is already following
+    # Loop through follower_list to check if requester is already following
     user_list = []
     for x in follower_list:
         follower_list = x.follower
@@ -92,7 +95,7 @@ def visitor_to_profile(request, username=None):
         follow_btn = 'unfollow'
     else:
         follow_btn = 'follow'
-        
+
     if username_obj.username == request.user.username:
         return redirect('profile')
     context = {
@@ -107,25 +110,27 @@ def visitor_to_profile(request, username=None):
         'city': username_obj.profile.city,
         'state': username_obj.profile.state,
     }
-    
+
     return render(request, 'profile_for_visitor.html', context)
+
 
 def followers_count(request):
     if request.method == 'POST':
-        #Get form values
+        # Get form values
         value = request.POST['value']
         following = request.POST['following']
         follower = request.POST['follower']
 
-        #If user is not following, create follower. Otherwise delete follower
+        # If user is not following, create follower. Otherwise delete follower
         if value == 'follow':
             followers_cnt = FollowersCount.objects.create(follower=follower, following=following)
             followers_cnt.save()
         else:
             followers_cnt = FollowersCount.objects.get(follower=follower, following=following)
             followers_cnt.delete()
-            
-        return redirect('/'+following)
+
+        return redirect('/' + following)
+
 
 # For logged in users to see their own profile page
 def profile(request):
@@ -152,7 +157,7 @@ def update_profile(request):
     if request.method == 'POST':
         p_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
         u_form = UpdateUserForm(request.POST, instance=request.user)
-        
+
         if p_form.is_valid() and u_form.is_valid():
             u_form.save()
             p_form.save()
@@ -163,21 +168,21 @@ def update_profile(request):
         u_form = UpdateUserForm(instance=request.user.profile)
 
     context = {
-            'p_form': p_form,
-            'u_form': u_form,
-            'current_user': request.user,
-            'profile_img': request.user.profile.profile_img,
-        }
+        'p_form': p_form,
+        'u_form': u_form,
+        'current_user': request.user,
+        'profile_img': request.user.profile.profile_img,
+    }
     return render(request, 'update_profile.html', context)
+
 
 @login_required()
 def update_security(request):
-    
     if request.method == 'POST':
-        #Get correct form
+        # Get correct form
         form = ChangePassword(request.user, request.POST)
 
-        #Check if form is valid, save and update if valid, display error if invalid
+        # Check if form is valid, save and update if valid, display error if invalid
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
@@ -189,20 +194,21 @@ def update_security(request):
         form = ChangePassword(request.user)
     return render(request, 'security_update.html', {'p_form': form, 'current_user': request.user})
 
+
 @login_required()
 def delete_profile(request):
     if request.method == 'POST':
-        #Get Profile object of user
+        # Get Profile object of user
         profile = Profile.objects.get(user=request.user)
 
-        #Remove profile image and qr code
+        # Remove profile image and qr code
         os.remove(profile.profile_img.path)
         os.remove(f'media/qr_code/{request.user.username}.jpg')
 
-        #Delete user data
+        # Delete user data
         profile.user.delete()
 
-        #Display success to user and return to homepage
+        # Display success to user and return to homepage
         messages.success(request, 'Your account was successfully deleted!')
         return redirect('welcome_index')
 
@@ -221,7 +227,8 @@ def register_request(request):
         # messages.error(request, f"{user_form.errors}")
     else:
         user_form = NewUserForm()
-    return render(request=request, template_name="register.html", context={"register_form": user_form, "user": request.user, "errors": user_form.errors})
+    return render(request=request, template_name="register.html",
+                  context={"register_form": user_form, "user": request.user, "errors": user_form.errors})
 
 
 def login_request(request):
