0413f7e626c723af37700051456dc5e5804624e7
Lucanatorr <collinslucas9@gmail.com>
2022-03-17 17:09:17 -0400

    Fixed follow button on show users page

--- welcome/urls.py
+++ welcome/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='welcome_index'),
     path('show-users/', views.show_all_users, name='show-all-users'),
+    path('show-users/followers_count', views.followers_count, name='followers_count'),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -1,7 +1,11 @@
+from pyexpat.errors import messages
+from django.http import QueryDict
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
 
 from user.models import FollowersCount
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
@@ -15,10 +19,47 @@ def show_all_users(request):
     list_of_active_nonstaff_users = user_model.objects.all().filter(is_active=True, is_staff=False)
     num_list = [1, 2, 3, 4, 5, 6]
 
+    user_list = get_user_followers(list_of_active_nonstaff_users, request).items()
+
+
+    print(user_list)
     context = {
         'current_user': current_user,
         'list_of_users': list_of_active_nonstaff_users,
         'num_list': num_list,
         'title': "People in your area",
+        'user_list': user_list,
     }
     return render(request, 'welcome/show_all_users.html', context)
+
+def get_user_followers(user_list: QueryDict, request):
+    user_dict= {}
+    for user in user_list:
+        follower = request.user.username
+        obj = FollowersCount.objects.filter(follower=follower, following=user.username)
+        if obj.exists():
+            user_dict.update({user: 'unfollow'})
+        else:
+            user_dict.update({user: 'follow'})
+    return user_dict
+
+
+@login_required
+def followers_count(request):
+    if request.method == 'POST':
+        # Get form values
+        value = request.POST['value']
+        following = request.POST['following']
+        follower = request.POST['follower']
+
+        # If user is not following, create follower. Otherwise delete follower
+        if value == 'follow':
+            followers_cnt = FollowersCount.objects.create(following=follower, follower=following)
+            followers_cnt.save()
+            messages.success(request, f'Now following {follower}')
+        else:
+            followers_cnt = FollowersCount.objects.get(following=follower, follower=following)
+            followers_cnt.delete()
+            messages.warning(request, f'Unfollowed {follower}')
+
+        return redirect('show-all-users')
