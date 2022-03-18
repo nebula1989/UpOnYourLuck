3f23989c121332e6147d34d57616259fb8ee57f9
nebula1989 <benwalterscoding@gmail.com>
2022-03-18 06:13:50 -0700

    removed code that maybe messing up qr urls

--- user/views.py
+++ user/views.py
@@ -259,14 +259,10 @@ def logout_request(request):
 
 
 def generate_qr_code(request):
-    try:
-        domain = DOMAIN
-        profile_url = request.user.profile.profile_url
-        user_profile_full_url = domain + '/' + profile_url + '/?source=qr'
-        qr_img = qrcode.make(user_profile_full_url)
-        qr_img.save(str(MEDIA_ROOT) + '/qr_code/' + request.user.username + '.jpg')
-        request.user.profile.qr_code_img = request.user.username + '.jpg'
-    except FileNotFoundError:
-        messages.error(request, "No path for file, going home")
-        return redirect("welcome_index")
+    domain = DOMAIN
+    profile_url = request.user.profile.profile_url
+    user_profile_full_url = domain + '/' + profile_url + '/?source=qr'
+    qr_img = qrcode.make(user_profile_full_url)
+    qr_img.save(str(MEDIA_ROOT) + '/qr_code/' + request.user.username + '.jpg')
+    request.user.profile.qr_code_img = request.user.username + '.jpg'
 
