1d068a81bbc1cf32b9126257fb7ba4e8ee3fc946
nebula1989 <benwalterscoding@gmail.com>
2022-03-23 18:12:56 -0700

    used app static folders for dev env

--- static/contact/app.js
+++
@@ -1,6 +0,0 @@
-let form = document.getElementById('contact-form');
-    let formElements = form.children;
-    // console.log(formElements.style);
-    for(ele of form.children) {
-        ele.style.marginTop = '2%';
-    }
