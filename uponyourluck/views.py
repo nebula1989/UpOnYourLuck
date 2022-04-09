5aa53994156ed117b9e368d14c1330cc6c8ecd61
arhughes11 <arhughes@my.waketech.edu>
2022-04-09 16:02:29 -0400

    changed server 500 method

--- uponyourluck/views.py
+++ uponyourluck/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
-def server_error_view(request, exception):
+def server_error_view(request, *args, **argv):
     return render(request, '500.html', status=500)    
