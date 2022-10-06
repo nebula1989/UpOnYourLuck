14cd67964267ef1bcbf6308197629bbbf3c615b2
nebula1989 <benwalterscoding@gmail.com>
2022-10-05 17:24:28 -0700

    fixed path

--- contact/views.py
+++ contact/views.py
@@ -111,8 +111,9 @@ def create_assessment(
         token: The token obtained from the client on passing the recaptchaSiteKey.
         recaptcha_action: Action name corresponding to the token.
     """
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'uponyourluck/settings/google_cred.json'
-
+    google_cred_path = '../uponyourluck/settings/google_cred.json'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = google_cred_path
+    path = '../uponyourluck/settings/google_cred.json'
     client = recaptchaenterprise_v1.RecaptchaEnterpriseServiceClient()
 
     # Set the properties of the event to be tracked.
